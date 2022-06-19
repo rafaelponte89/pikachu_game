b4f779d226a2d108cff060fda4692809e62580c2
Rafael Ponte Cassemiro <rafael_ponte1b@hotmail.com>
2022-06-19 13:07:44 -0300

    add controls-mobile

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const power = document.querySelector('.power');
 
 /*Declaração de função anônima*/
 const jump = () => {
-    
+
     character.classList.add('jump-character');
 
     setTimeout(() => {
@@ -24,41 +24,77 @@ const thunderPower = () => {
 };
 
 
+function colision() {
+    
+}
 
-const loopGame = setInterval( () => {
+const loopGame = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const characterPosition = +window
         .getComputedStyle(character)
-        .bottom.replace("px","");
+        .bottom.replace("px", "");
+    console.log(window.innerWidth);
 
-    if (pipePosition <= 120 && pipePosition > 0 && characterPosition < 80) {
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+    if (window.innerWidth > 380) {
+        if (pipePosition <= 120 && pipePosition > 0 && characterPosition < 80) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
 
-        character.style.animation = 'none';
-        character.style.bottom = `${characterPosition}px`;
+            character.style.animation = 'none';
+            character.style.bottom = `${characterPosition}px`;
 
-        character.style.backgroundImage= 'url("./images/pikachu-defeated.png")';
-    
-        character.style.width = '100px';
-        character.style.marginLeft='45px';
+            character.style.backgroundImage = 'url("./images/pikachu-defeated.png")';
 
-        power.style.animation='none';
+            character.style.width = '100px';
+            character.style.marginLeft = '45px';
 
-     
-        clearInterval(loopGame);
+            power.style.animation = 'none';
+
+
+            clearInterval(loopGame);
+        }
     }
-},10);
+    else {
+        if (pipePosition <= 60 && pipePosition > 0 && characterPosition < 40) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePosition}px`;
 
-const eventoTecla = document.addEventListener('keydown',function(event) {
-   
+            character.style.animation = 'none';
+            character.style.bottom = `${characterPosition}px`;
+
+            character.style.backgroundImage = 'url("./images/pikachu-defeated.png")';
+
+            character.style.width = '50px';
+            character.style.marginLeft = '27.5px';
+
+            power.style.animation = 'none';
+
+
+            clearInterval(loopGame);
+        }
+    }
+}, 10);
+
+
+function controlMobile() {
+    const pulo = document.getElementById('jump');
+    const raio = document.getElementById('thunder');
+    pulo.addEventListener('click', jump, false);
+    raio.addEventListener('click', thunderPower, false);
+}
+
+controlMobile();
+
+const eventoTecla = document.addEventListener('keydown', function (event) {
+
     let tecla = event.code;
     console.log(tecla);
 
-    if (tecla == 'ArrowUp'){
+    if (tecla == 'ArrowUp') {
         jump(event);
     }
     else if (tecla == 'Enter') {
-       thunderPower(event);
+        thunderPower(event);
     }
 });
+
